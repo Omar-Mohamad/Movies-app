25eca47f44a483b5d2b1eb9d47dd8080061f9613
Omar Mohammed <omarmohammed555@hotmail.com>
2021-05-21 22:34:29 +0200

    finished the project

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ const form = document.querySelector("#search-form"),
   topRatedMoviesContainer = document.querySelector("#toprated"),
   upCommingMoviesContainer = document.querySelector("#upcomming");
 
+const contentContainer = document.querySelector(".content-container");
+
+const searchValue = document.querySelector(".search-info");
+
+const resultsContainer = document.querySelector(".results-container");
+
 // API data
 const topRatedUrl =
     "https://api.themoviedb.org/3/movie/top_rated?api_key=ec002527d6c6ee4ad05b7a57a673a4c6",
@@ -30,6 +36,9 @@ const topRatedUrl =
     "https://api.themoviedb.org/3/movie/popular?api_key=ec002527d6c6ee4ad05b7a57a673a4c6",
   nowPlayingUrl =
     "https://api.themoviedb.org/3/movie/now_playing?api_key=ec002527d6c6ee4ad05b7a57a673a4c6";
+
+const searchUrl =
+  "https://api.themoviedb.org/3/search/movie?api_key=ec002527d6c6ee4ad05b7a57a673a4c6&query=";
 
 // Getting the Now playing movies
 
@@ -208,4 +217,88 @@ function loadUpcomming() {
     .catch(function (error) {
       console.log("Rejected:", error.message);
     });
+}
+
+form.addEventListener("submit", changePath);
+
+async function searchMovie() {
+  const searchedMovieResponse = await fetch(searchUrl + searchField.value);
+
+  if (searchedMovieResponse.status !== 200) {
+    if (searchField.value === "") {
+      alert("Please enter the movie name that you want to search for");
+    }
+    throw new Error("The searched movie is not found.");
+  }
+
+  const searchedMovie = searchedMovieResponse.json();
+
+  return searchedMovie;
+}
+
+function changePath(e) {
+  e.preventDefault();
+
+  clearHomeContent();
+
+  clearContent();
+
+  searchMovie()
+    .then(function (data) {
+      searchValue.textContent = "Results for : " + searchField.value;
+
+      for (let i = 0; i < data.results.length; i++) {
+        const movieContainer = document.createElement("div"),
+          moviePoster = document.createElement("img"),
+          movieTitle = document.createElement("h3"),
+          movieRating = document.createElement("P");
+
+        movieContainer.setAttribute("class", "searched-movie");
+        moviePoster.setAttribute("class", "movie-poster");
+
+        if (data.results[i].poster_path) {
+          moviePoster.setAttribute(
+            "src",
+            "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path
+          );
+        } else {
+          movieContainer.style.display = "none";
+        }
+
+        movieTitle.setAttribute("class", "title");
+        movieRating.setAttribute("class", "rating");
+
+        if (data.results[i].title.length > 17) {
+          movieTitle.textContent = data.results[i].title.slice(0, 17) + "...";
+
+          movieTitle.setAttribute("title", data.results[i].title);
+        } else {
+          movieTitle.textContent = data.results[i].title;
+        }
+
+        movieRating.textContent = data.results[i].vote_average + "/10";
+
+        movieContainer.appendChild(moviePoster);
+        movieContainer.appendChild(movieTitle);
+        movieContainer.appendChild(movieRating);
+
+        resultsContainer.appendChild(movieContainer);
+      }
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log("Rejected:", error.message);
+    });
+}
+
+function clearHomeContent() {
+  bannerContainer.style.display = "none";
+
+  contentContainer.style.display = "none";
+}
+
+function clearContent() {
+  while (resultsContainer.hasChildNodes()) {
+    resultsContainer.lastElementChild.remove();
+  }
 }
